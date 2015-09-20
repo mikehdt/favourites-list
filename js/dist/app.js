3b96a29b72fa2094147c08a10d21d9e94d91c81a
Mike Hopkins <mike@monkii.com>
2015-09-20 19:25:26 +1000

    Improve iOS a touch; remove translucent highlight on tap

--- js/dist/app.js
+++ js/dist/app.js
@@ -297,6 +297,9 @@ $(function(){
     // Implemente FastClick to remove 300ms tap delay
     FastClick.attach(document.body);
 
+    // Allow iOS to use :active styles (just in case)
+    document.addEventListener('touchstart', function(){}, true);
+
     // Bootstrap the app view
     new app.AppView();
 });
